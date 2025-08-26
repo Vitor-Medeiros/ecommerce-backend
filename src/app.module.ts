a217f0cbcb6f3d36f16808a732950a3563a894b5
Vitor-Medeiros <vitorbortot@gmail.com>
2025-08-25 21:44:38 -0300

    fix: Atualiza configuração do TypeOrm para usar variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './cases/categories/category.module';
 import { BrandModule } from './cases/brands/brand.module';
 import { ProductModule } from './cases/products/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'aws-0-sa-east-1.pooler.supabase.com',
-      port: +'5432',
-      username: 'postgres.iynwpnyicrsljdrvtvsj',
-      password: 'Bortot123#',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
